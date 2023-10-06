5cabee59bfd19a009aa9d9bafd0bfd906be1b556
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-06 21:04:38 +0400

    04_05) 1 Update main

--- 04/04.cpp
+++ 04/04.cpp
@@ -11,4 +11,20 @@ public:
 	int Count;
 };
 
-int main() {};
+int main() {
+	const int s = 3;
+	TBook books[s];
+	ifstream fin;
+	fin.open("book.txt");
+	if (fin.is_open()) {
+		for (int i = 0; i < s; i++) {
+			string temp;
+			getline(fin, books[i].Name);
+			getline(fin, books[i].Author);
+			getline(fin, temp);
+			books[i].Count = stoi(temp);
+		}
+		fin.close();
+	}
+	else return 1;
+};
