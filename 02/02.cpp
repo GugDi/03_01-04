46851dfbbafb5e9252f5950f8a11ba046b4204d0
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-06 20:40:54 +0400

    02_08) Создание конструктора с параметрами Road()

--- 02/02.cpp
+++ 02/02.cpp
@@ -3,10 +3,15 @@ using namespace std;
 class Road {
 private:
 	int length, width;
-}
+public:
+	Road(int newlength, int newwidth) {
+		length = newlength;
+		width = newwidth;
+	}
+};
 class Car {
 private:
 	double X, V;
 	int P;
-}
+};
 int main() {};
