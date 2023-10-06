992bd662d1e4b6413cb45c063d96052032717082
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-06 20:48:17 +0400

    02_13) Оформление main для CLS + оформление кода

--- 02/02.cpp
+++ 02/02.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
+
 using namespace std;
+
 class Road {
-private:
-	int length, width;
-public:
-	Road(int newlength, int newwidth) {
-		length = newlength;
-		width = newwidth;
-	}
-	void Print() {
-		cout << "Length: " << length << endl;
-		cout << "Width: " << width << endl;
-	}
+	private:
+		int length, width;
+	public:
+		Road(int newlength, int newwidth) {
+			length = newlength;
+			width = newwidth;
+		}
+		void Print() {
+			cout << "Length: " << length << endl;
+			cout << "Width: " << width << endl;
+		}
 };
 class Car {
-private:
-	double X, V;
-	int P;
-public:
-	Car(double newx, int newp, double newv) {
-		X = newx;
-		P = newp;
-		V = newv;
-	}
-	void Print() {
-		cout << "X: " << X << endl;
-		cout << "P: " << P << endl;
-		cout << "V: " << V << endl;
-	}
+	private:
+		double X, V;
+		int P;
+	public:
+		Car(double newx, int newp, double newv) {
+			X = newx;
+			P = newp;
+			V = newv;
+		}
+		void Print() {
+			cout << "X: " << X << endl;
+			cout << "P: " << P << endl;
+			cout << "V: " << V << endl;
+		}
 };
 int main() {
-	Road road1(4, 10);
-	road1.Print();
-	Car car1(0, 2, 2);
-	car1.Print();
+	cout << "Road length and width:" << endl;
+		Road road1(4, 10);
+		road1.Print();
+	cout << "Car position:" << endl;
+		Car car1(0, 2, 2);
+		car1.Print();
 	return 0;
 }
+
